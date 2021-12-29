4c4877d4bddf7f7d8e105c9456d58600dcc47b7e
KlaraMil <klaramilosicova@seznam.cz>
2021-12-29 20:28:41 +0100

    Commit Klara: vkladani cisel

--- #projektKlara.py
+++ #projektKlara.py
@@ -1,1 +1,11 @@
 #projektKlara
+# 1. Vygeneruj nahodne x,y cisel random
+        # 2. Zadaj v terminale min 5 cisel
+        
+x = input('Vložte minimálně 5 celých čísel oddělených mezerami: ')
+print("\n")
+cisla_uzivatele = x.split()
+print('seznam čísel: ', cisla_uzivatele)
+
+print ("Nejvyšší hodnota v seznamu: ", max(cisla_uzivatele))
+print ("Nejmenší hodnotu v seznamu: ", min(cisla_uzivatele))
