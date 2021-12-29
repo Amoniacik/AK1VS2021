e7450e44e9aeabb3a9471dbdedaf794909b7720d
Amoniacik <kulasova.monika@gmail.com>
2021-12-29 15:58:27 +0100

    Priprava na zadanie

--- #Projekt Monika.py
+++ #Projekt Monika.py
@@ -1,1 +1,17 @@
 #Projekt Monika
+
+print("Hello, World!")
+
+#nacitanie premennej:
+x = 5
+
+#vypisanie premennej do konzoly:
+print(x)
+
+#kombinacia textu a premennej(+ pretypovanie z intu na string):
+print("Hodnota premennej x je : " + str(x))
+
+#nacitanie zo vstupu premennych:
+
+
+
