8344903eb00395ef17755c8fa222688a4d1c241a
KlaraMil <klaramilosicova@seznam.cz>
2022-01-02 20:03:29 +0100

    doplneni komentaru do fcie main

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import fcie_temp;
 
-print('Zvol cislo 1: Vygeneruj nahodne 20 cisel random')
-print('Zvol cislo 2: Vložte libovolný počet celých čísel oddělených mezerami')
-print('Zvol cislo 3: Nacitaj zo suboru cisla')
+print('Zvol cislo 1: Vygeneruj nahodne 20 cisel random  \n')
+print('Zvol cislo 2: Vložte libovolný počet celých čísel oddělených mezerami  \n')
+print('Zvol cislo 3: Nacitaj zo suboru cisla  \n')
 
 choice = int(input('Enter your choice:'))
 
 if (choice == 1):
-    print('Choice 1: Vygeneruj nahodne 20 cisel random: ')
+    print('Choice 1: Vygeneruj nahodne 20 cisel random \n')
 
     import random
     nahodna_cisla = random.sample(range(-50, 50), 20)
@@ -27,7 +27,6 @@ if (choice == 2):
 if (choice == 3):
     print('Nacitaj zo suboru cisla \n')
 
-    #Nacitanie suboru file.txt a rozparsovanie cisel oddelenych ciarkou
     with open("file.txt", "r") as tf:
         cisla = tf.read().split(',')    
 
