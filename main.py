0a2d89bb94ba8ece384c72fd2a9f988f44089fda
KlaraMil <klaramilosicova@seznam.cz>
2022-01-02 20:12:36 +0100

    komentar k main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import fcie_temp;
 
+# Uživatel zvolí, o jaké pole čísel se bude jednat
+
 print('Zvol cislo 1: Vygeneruj nahodne 20 cisel random  \n')
 print('Zvol cislo 2: Vložte libovolný počet celých čísel oddělených mezerami  \n')
 print('Zvol cislo 3: Nacitaj zo suboru cisla  \n')
 
 choice = int(input('Enter your choice:'))
+
+# Program vytvoří pole náhodných čísel
 
 if (choice == 1):
     print('Choice 1: Vygeneruj nahodne 20 cisel random \n')
@@ -13,6 +17,8 @@ if (choice == 1):
     nahodna_cisla = random.sample(range(-50, 50), 20)
     
     print(nahodna_cisla)
+
+# Uživatel nahraje jednotlivá čísla do programu
 
     fcie_temp.vyber_algorythm(nahodna_cisla)
 if (choice == 2):
@@ -24,6 +30,9 @@ if (choice == 2):
     cisla_uzivatele = x.split()
     print('Seznam čísel: ', cisla_uzivatele)
     fcie_temp.vyber_algorythm(cisla_uzivatele)
+
+# Program vybere čísla z textového dokumentu
+
 if (choice == 3):
     print('Nacitaj zo suboru cisla \n')
 
